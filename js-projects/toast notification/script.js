f26de83fc1c1e46139254c0944497b0f14cf2b46
mitul upadhyay <mitulupadhyay0910@gmail.com>
2026-08-30 21:38:25 +0530

    feat: add toast notifications

---
+++ js-projects/toast notification/script.js
@@ -0,0 +1,26 @@
+let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please Fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid Input,Check again!';
+
+
+
+
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        toast.remove();
+    },2500);
+
+}
