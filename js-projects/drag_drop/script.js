7ee9948396fb38ddb280abc9ff6afeaa2a1ca34e
mitul upadhyay <mitulupadhyay0910@gmail.com>
2026-09-04 22:18:12 +0530

    feat: add drag and drop functionality

---
+++ js-projects/drag_drop/script.js
@@ -0,0 +1,33 @@
+let lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+let selected = null;
+
+for (let list of lists) {
+    list.addEventListener("dragstart", function (e) {
+        selected = e.target;
+    });
+}
+
+rightBox.addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+
+rightBox.addEventListener("drop", function () {
+    if (selected) {
+        rightBox.appendChild(selected);
+        selected = null;
+    }
+});
+
+leftBox.addEventListener("dragover", function (e) {
+    e.preventDefault();
+});
+
+leftBox.addEventListener("drop", function () {
+    if (selected) {
+        leftBox.appendChild(selected);
+        selected = null;
+    }
+});
